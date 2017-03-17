1917277f3e58ff87ffdff6e16e8fd8b8db5295ad
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-17 15:20:35 +0100

    Standardization

--- js/configureStore.js
+++ js/configureStore.js
@@ -3,6 +3,7 @@ import devTools from 'remote-redux-devtools';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
+
 import reducer from './reducers';
 import promise from './promise';
 
