adf4d4b3503cfab2edac7c3a9c3a750ebfbd6266
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-21 17:43:18 +0100

    Adds all profile reducer

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,9 +2,18 @@ import { combineReducers } from 'redux';
 
 import drawer from './drawer';
 import cardNavigation from './cardNavigation';
+import experiences from './experiences';
+import educations from './educations';
+import skills from './skills';
+import interests from './interests';
+import languages from './languages';
 
 export default combineReducers({
   drawer,
   cardNavigation,
-
+  experiences,
+  educations,
+  skills,
+  interests,
+  languages,
 });
