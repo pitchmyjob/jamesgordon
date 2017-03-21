a9d209ea2807c03412ae29ddffd93109c3880178
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-21 17:42:42 +0100

    Removes old promise utils to use redux middleware promise

--- js/configureStore.js
+++ js/configureStore.js
@@ -3,13 +3,13 @@ import devTools from 'remote-redux-devtools';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
+import promise from 'redux-promise-middleware'
 
 import reducer from './reducers';
-import promise from './promise';
 
 export default function configureStore(onCompletion:()=>void):any {
   const enhancer = compose(
-    applyMiddleware(thunk, promise),
+    applyMiddleware(promise(), thunk),
     devTools({
       name: 'nativestarterkit', realtime: true,
     }),
