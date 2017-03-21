623f3312c836cc88369b12874308ce1d80b978eb
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-21 18:36:59 +0100

    Adds constants, actions & reducer to handle destroy interest

--- js/actions/interests.js
+++ js/actions/interests.js
@@ -1,10 +1,20 @@
 import request from '../utils/request'
 
-import { LIST_INTEREST } from '../constants/interests'
+import { LIST_INTEREST, DESTROY_INTEREST } from '../constants/interests'
 
 export const listInterest = () => {
     return {
         type: LIST_INTEREST,
         payload: request.get('/applicantinterests/')
+    }
+}
+
+export const destroyInterest = (id) => {
+    return {
+        type: DESTROY_INTEREST,
+        payload: request.delete('/applicantinterests/' + id + '/'),
+        meta: {
+            id: id,
+        },
     }
 }
--- js/reducers/interests.js
+++ js/reducers/interests.js
@@ -1,17 +1,30 @@
-import { LIST_INTEREST_PENDING, LIST_INTEREST_FULFILLED, LIST_INTEREST_REJECTED } from '../constants/interests'
+import { LIST_INTEREST_PENDING, LIST_INTEREST_FULFILLED, LIST_INTEREST_REJECTED,
+         DESTROY_INTEREST_PENDING, DESTROY_INTEREST_FULFILLED, DESTROY_INTEREST_REJECTED
+} from '../constants/interests'
 
 const INITIAL_STATE = {
     interestList: {pending: false, fulfilled: false, error: null, interests: []},
+    interestActive: {pending: false, fulfilled: false, error: null, interest: null},
 }
 
 export default (state=INITIAL_STATE, action) => {
     switch (action.type) {
+        // LIST
         case LIST_INTEREST_PENDING:
             return {...state, interestList: {pending: true, fulfilled: false, error: null, interests: []}}
         case LIST_INTEREST_FULFILLED:
             return {...state, interestList: {pending: false, fulfilled: true, error: null, interests: action.payload.data}}
         case LIST_INTEREST_REJECTED:
             return {...state, interestList: {pending: false, fulfilled: false, error: action.payload.response, interests: [], pagination: null}}
+
+        // DESTROY
+        case DESTROY_INTEREST_PENDING:
+            return {...state, interestActive: {pending: true, fulfilled: false, error: null, interest: state.interestList.interests.find((interest) => { return interest.id === action.meta.id })}}
+        case DESTROY_INTEREST_FULFILLED:
+            return {...state, interestActive: {pending: false, fulfilled: true, error: null, interest: null}, interestList: {...state.interestList, interests: state.interestList.interests.filter((interest) => { return interest.id !== action.meta.id })}}
+        case DESTROY_INTEREST_REJECTED:
+            return {...state, interestActive: {pending: false, fulfilled: false, error: action.payload.response, interest: null}}
+
         default:
             return state
     }
