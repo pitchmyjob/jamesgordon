652b8b0eeb75b096a2a2051b847e86883ae69c56
Maximilien Raulic <raulic.maximilien@gmail.com>
2017-03-21 18:26:47 +0100

    Adds constants, actions & reducer to handle destroy skill

--- js/actions/skills.js
+++ js/actions/skills.js
@@ -1,10 +1,20 @@
 import request from '../utils/request'
 
-import { LIST_SKILL } from '../constants/skills'
+import { LIST_SKILL, DESTROY_SKILL } from '../constants/skills'
 
 export const listSkill = () => {
     return {
         type: LIST_SKILL,
         payload: request.get('/applicantskills/')
+    }
+}
+
+export const destroySkill = (id) => {
+    return {
+        type: DESTROY_SKILL,
+        payload: request.delete('/applicantskills/' + id + '/'),
+        meta: {
+            id: id,
+        },
     }
 }
