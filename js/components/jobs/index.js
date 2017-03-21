8dc714b26a286df4d5cf5da9fe6b61bd0ca5227c
Yannis Tannier <yannistannier@imac-de-martial.home>
2017-03-21 10:59:33 +0100

    add jobs

--- js/components/jobs/index.js
+++ js/components/jobs/index.js
@@ -1,9 +1,9 @@
 
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import { connect } from 'react-redux';
 import { actions } from 'react-native-navigation-redux-helpers';
-import { Container, Content, Button, Item, Icon, Text, Input } from 'native-base';
+import { Container, Content, Button, Item, Icon, Text, Input, Form, Card } from 'native-base';
 
 
 import HeaderContent from './../headerContent';
@@ -11,6 +11,8 @@ import theme from '../../themes/base-theme';
 import styles from './styles';
 
 const { popRoute } = actions;
+
+const bg = require('../../../images/bg.png');
 
 class Jobs extends Component { // eslint-disable-line
 
@@ -20,33 +22,90 @@ class Jobs extends Component { // eslint-disable-line
     }),
   }
 
+  constructor(props){
+    super(props);
+    this.state = {
+      poste: '',
+      postes:[]
+    }
+  }
+
+  _handleKeyPress(){
+
+      if(!this.state.postes.includes(this.state.poste))
+      {
+        var arrayvar = this.state.postes.slice()
+        arrayvar.push(this.state.poste)
+        this.setState({ postes: arrayvar })
+
+      }
+      this.setState({ poste: '' })
+  }
+
+  deleteItem(index){
+    alert(index)
+
+  }
+
   render() { // eslint-disable-line class-methods-use-this
     return (
       <Container>
-        <View style={styles.container}>
-          <HeaderContent />
-          <Content>
-            <Text>Mes postes</Text>
-            <Item>
-              <Icon name="search" />
-              <Input
-                placeholder="Saisissez un poste recherché"
-                secureTextEntry
-                placeholderTextColor="#fff"
-                onChangeText={password => this.setState({ password })}
-                style={styles.input}
-              />
-              <Button
-                primary block rounded
-                onPress={() => this.replaceRoute('home')}
-              >
-                <Text>
-                  Suivant
-                </Text>
-              </Button>
-            </Item>
-          </Content>
-        </View>
+        <Image source={bg} style={styles.background} >
+            <Content style={styles.content}>
+                <View style={styles.container}>
+                <Text style={styles.title}>Mes postes</Text>
+
+                <Form>
+                  <Item rounded style={styles.inputGrp}>
+                    <Icon name="search" />
+                    <Input
+                      onSubmitEditing={this._handleKeyPress.bind(this)}
+                      onChangeText={poste => this.setState({ poste })}
+                      placeholder="Saisissez un poste recherché"
+                      placeholderTextColor="#fff"
+                      style={styles.input}
+                      value={this.state.poste}
+                    />
+                  </Item>
+                </Form>
+
+
+                
+
+                {this.state.postes.map(function(name, index){
+                    return (
+                        <Item
+                          key={index}
+                          block center
+                          style={styles.itemPoste}>
+                        
+                          <Text>{name}</Text>
+
+                          <Button style={styles.itemPosteClose} onPress={() => { 
+                              const list = this.state.postes.filter((poste, indexList) => {
+                                return indexList !== index
+                              })
+                              this.setState({postes: list});
+                          }}>
+                              <Icon name="close" />
+                          </Button>
+                        </Item>
+
+                      )
+                  }.bind(this))}
+
+
+                <Button
+                    style={styles.buttonSuivant}
+                    primary block rounded
+                    onPress={() => this.replaceRoute('home')} >
+                     <Text>suivant</Text>
+                </Button>
+
+
+              </View>
+            </Content>
+          </Image>
       </Container>
     );
   }
--- js/components/jobs/styles.js
+++ js/components/jobs/styles.js
@@ -1,14 +1,67 @@
 
 const React = require('react-native');
 
+
 const { StyleSheet, Dimensions, Platform } = React;
+
+const primary = require('../../themes/variable').brandPrimary;
+const deviceHeight = Dimensions.get('window').height;
+const deviceWidth = Dimensions.get('window').width;
+
 
 // const deviceHeight = Dimensions.get('window').height;
 
 export default {
+  signupContainer: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginTop: (deviceWidth < 330) ? (Platform.OS === 'android' ? ((deviceHeight / 9) - 10) : ((deviceHeight / 9) - 10)) : (Platform.OS === 'android' ? ((deviceHeight / 14) - 10) : ((deviceHeight / 10) - 10)),
+  },
+  title:{
+  	marginTop:20,
+  	fontWeight: 'bold',
+    fontSize: 18,
+  	textAlign: 'center',
+  },
+  buttonSuivant: {
+  	marginLeft:20,
+  	marginRight:20,
+  },
+  content:{
+  	marginTop:20,
+  },
   container: {
     flex: 1,
     width: null,
     height: null,
+  },
+  background: {
+    flex: 1,
+    width: null,
+    height: null,
+    backgroundColor: primary,
+  },input: {
+    paddingLeft: 10,
+    color: '#AAA',
+  },
+  itemPoste:{
+  	backgroundColor: '#AAA',
+  	textAlign: 'center',
+  	flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:10
+  },
+  itemPosteClose:{
+  	backgroundColor: '#AAA',
+  },
+  inputGrp: {
+    flexDirection: 'row',
+    borderRadius: 25,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    marginBottom: 20,
+    borderWidth: 0,
+    marginTop:30,
+    borderColor: 'transparent',
   },
 };
